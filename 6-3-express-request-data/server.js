55097ae39f587a0f6095b6bbc9cbed1f946c796b
maryamsaladsani <s202263480@kfupm.edu.sa>
2025-11-12 11:42:07 +0300

    TODO1 TODO2 TODO3 TODO4

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -134,8 +134,22 @@ app.get("/profile/:first/:last", (req, res) => {
  });
 });
 
-// Route param middleware example: /users/42
+// TODO04 : Route param middleware example: /users/42
+app.param("userId", (req, res, next, userId) => {
+ const userIdNum = Number(userId);
 
+ // validate: must be a positive number
+ if (isNaN(userIdNum) || userIdNum <= 0) {
+  return res.status(400).json({
+   ok: false,
+   error: "userId must be positive number",
+  });
+ }
+
+ // store numeric value for later routes
+ req.userIdNum = userIdNum;
+ next();
+});
 
 // Route params: /users/:userId route
 
